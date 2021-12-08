d5872168569e8540368693946e28f3a26c1acba7
jzazooro <91785177+jzazooro@users.noreply.github.com>
2021-12-08 18:21:26 +0100

    subida

---
+++ laberinto.py
@@ -0,0 +1,44 @@
+laberinto= [
+    ['E', 'M', 'M', 'M', 'M'],
+    ['C', 'M', 'C', 'C', 'C'],
+    ['C', 'M', 'C', 'M', 'C'],
+    ['C', 'C', 'C', 'M', 'C'],
+    ['M', 'M', 'M', 'M', 'S']
+    ]
+x=0
+y=0
+print((laberinto[y])[x])
+print("Te encuentras en la entrada, posicion (0, 0)")
+caminorecorrido=["", "", "", "", "", "", "", "", "", "", "", "", ""]
+i=0
+while (laberinto[y])[x] == 'C' or 'E':
+    eleccion= input("Por favor seleccione cual sera su siguiente movimiento: izquierda, derecha, arriba, abajo: ")
+    if eleccion== "izquierda" or "derecha" or "arriba" or "abajo":
+        print("has seleccionado {}".format(eleccion))
+        caminorecorrido[i]=eleccion
+        i=i+1
+    if eleccion=="izquierda":
+        x=x-1
+        y=y
+    if eleccion=="derecha":
+        x=x+1
+        y=y
+    if eleccion=="arriba":
+        x=x
+        y=y+1
+    if eleccion=="abajo":
+        x=x
+        y=y-1
+    else:
+        print("Lo siento, no has escogido una de las cuatro opciones posibles")
+        x=x
+        y=y
+    print("te encuentras en la casilla ({},{})".format(x,y))
+    if(laberinto[y])[x]=='C' or 'E':
+        print("Puedes continuar en el laberinto")
+    elif(laberinto[y])[x]=='S':
+        print("Enhorabuena has conseguido salir del laberinto")
+        break
+    else:
+        print("Te has chocado con un muro \nreinicia el programa para poder completar el laberinto")
+print("Has realizado los siguientes movimientos: ", (caminorecorrido))
